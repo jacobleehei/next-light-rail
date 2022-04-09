fe6654c630a6d760fa3d8ca39557120f1d045caf
Jacob Lee <jacob.lee@winpromise.com>
2022-04-09 19:30:01 +0800

    Add NextTrainTable

---
+++ public/src/components/nextTrainTable.js
@@ -0,0 +1,32 @@
+import DataTable from '../dataDisplay/table';
+
+export default function NextTrainTable({
+   rows,
+}) {
+
+   return rows.length !== 0 && <DataTable
+      rows={rows.map((s, idx) => {
+         return {
+            ...s,
+            id: idx,
+         }
+      })}
+      columns={[
+         {
+            field: 'code',
+            headerName: 'Code',
+            width: 90,
+         },
+         {
+            field: 'titleZh',
+            headerName: 'titleZh',
+            width: 90,
+         },
+         {
+            field: 'titleEn',
+            headerName: 'titleEn',
+            width: 90,
+         },
+      ]}
+   />
+}
