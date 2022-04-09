205d93867201d2ef3b4d09c92721ca4533b9c280
Jacob Lee <jacob.lee@winpromise.com>
2022-04-09 19:29:07 +0800

    all route functions move to dir "routes"

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3001
-const lightRailInformation = require('./src/light-rail-information')
-const lightRailInformationByStation = require('./src/light-rail-by-station')
-app.get('/', (req, res) => {
-   res.send('Hello World!')
-})
+const api = require('./routes/api')
 
-app.get('/api/lightrail/json', (req, res) => {
-   const code = req.query.code
-   if (code) {
-      const station = lightRailInformationByStation[code]
-      return station ?
-         res.json(lightRailInformationByStation[code])
-         :
-         res.status(400).json({ error: 'Not Found' })
-   }
-
-   return res.json(lightRailInformation)
-})
+app.use(cors())
+app.get('/', api.main)
+app.get('/api/lightRail/json', api.getLightRailJson)
 
 app.listen(port, () => {
    console.log(`Next-Light-Rail App Listening on Port ${port}`)
