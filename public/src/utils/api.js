a0d121a98ceda669bdb8a7be6eabc9fdd06945fe
Jacob Lee <jacob.lee@winpromise.com>
2022-04-09 19:30:52 +0800

    Add function getLightRailStation

---
+++ public/src/utils/api.js
@@ -0,0 +1,15 @@
+import { minDistancePoints } from '../utils/math';
+
+export function getLightRailStation(setLightRailStation, geolocation) {
+   fetch('http://localhost:3001/api/lightRail/json')
+      .then(res => {
+         return res.json()
+      })
+      .then(json => {
+         const sortedLightRail = minDistancePoints({
+            targetPoint: geolocation,
+            comparePoints: json
+         })
+         setLightRailStation(sortedLightRail)
+      })
+}
