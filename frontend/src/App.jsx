bbaef9b563fbd9b694e2d392a6aed3dba0e716d3
jacobleehei <jasonleehei@gmail.com>
2022-07-06 03:02:54 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getGeolocation } from './utils/getGeolocation';
-import NextTrainTable from './components/nextTrainTable.jsx';
-import { getLightRailStation } from './utils/getLightRailStation';
+import { getGeolocation } from './utils/getGeolocation.js';
+import NextTrainTable from './components/NextTrainTable.jsx';
+import { getLightRailStation } from './utils/getLightRailStation.js';
+import BottomAppMenu from './components/BottomAppMenu';
 
 function App() {
     const [geolocation, setGeolocation] = useState({})
     const [lightRailStation, setLightRailStation] = useState([])
 
-    useEffect(() => {
-        getGeolocation(setGeolocation)
-    }, [])
-
-    useEffect(() => {
-        getLightRailStation(setLightRailStation, geolocation)
-    }, [geolocation])
+    useEffect(() => getGeolocation(setGeolocation), [])
+    useEffect(() => getLightRailStation(setLightRailStation, geolocation), [geolocation])
 
 
     return (
         <div className="App" >
-            <NextTrainTable rows={lightRailStation} />
+            <BottomAppMenu />
         </div>
     );
 }
