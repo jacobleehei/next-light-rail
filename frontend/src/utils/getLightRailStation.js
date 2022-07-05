e0438c536653337f9d4d7a56501ac24e15fb9d05
jasonleehei <74966325+jasonleehei@users.noreply.github.com>
2022-07-06 02:04:32 +0800

    add error checking

---
+++ frontend/src/utils/getLightRailStation.js
@@ -0,0 +1,21 @@
+import { minDistancePoints } from './math';
+
+export function getLightRailStation(setLightRailStation, geolocation) {
+    if (!setLightRailStation) {
+        throw new Error("setLightRailStation must be defined");
+    }
+    if (!geolocation) {
+        throw new Error("geolocation must be defined");
+    }
+
+    // Fetch the light rail data from backend
+    fetch('http://localhost:3001/api/lightRail/json')
+        .then(res => res.json())
+        .then(json => {
+            const sortedLightRail = minDistancePoints({
+                targetPoint: geolocation,
+                comparePoints: json
+            })
+            setLightRailStation(sortedLightRail)
+        })
+}
